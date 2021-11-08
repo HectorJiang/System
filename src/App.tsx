b8f14a66058fa6a64a5cdb7ce1b8acdeeb672fc3
Hector Jiang <hector.jiang@outlook.com>
2021-11-09 04:09:02 +0800

    update system

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
 import * as React from 'react'
 import Hello from  './components/Hello'
-
-//           /\
-//          /**\
-//         /****\   /\
-//        /      \ /**\
-//       /  /\    /    \        /\    /\      /\
-//      /  /  \  /      \      /  \/\/  \    /  \/\                           -------- __@
-//     /  /    \/ /\     \    /    \ \   \  /   /  \       LIFE IS SHORT      ----- _`\<,_
-//    /  /      \/  \/\   \  /      \     \         \      KEEP FORWARD         ---- (*)/ (*)
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import PositionedMenu from './components/Header'
+import OutlinedCard from './components/Content'
 
 class App extends React.Component{
     public render() {
         return(
             <div className="App">
+                <PositionedMenu />
+                <OutlinedCard />
                 Hello React Typescript
                 <Hello title="标题" age={21}/>
             </div>
