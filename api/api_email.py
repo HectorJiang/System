930f043900d0609c1d3fba67e46c82cc3bff92a6
hectorjiang <jiangyongxu1208@gmail.com>
2022-07-10 23:25:16 +0800

    chore(arch):change arch

--- api/api_email.py
+++ api/api_email.py
@@ -1,10 +1,12 @@
 '''
 Author: Hector Jing
 Date: 2022-07-09 12:26:33
-LastEditTime: 2022-07-09 13:45:28
+LastEditTime: 2022-07-10 21:55:06
 Description:
 '''
 import json
+
+from sympy import content
 with open("rules.json", "r", encoding="utf8") as f:    
     jsonObj = json.load(f)
     
@@ -19,20 +21,27 @@ email_server = jsonObj["email_server"]
 email_server_port = jsonObj["email_server_port"]
 email_user = jsonObj["email_user"]
 email_pass = jsonObj["email_pass"]
+smtpObj = smtplib.SMTP(email_server, email_server_port)
+smtpObj.login(email_user, email_pass)
 
-sender = jsonObj["email_user"]
-receivers = jsonObj["receivers"]
 
-message = MIMEText("内容", 'plain', 'utf-8')
-message['From'] = Header(sender, 'utf-8')
-message['To'] = Header("Hector.Jiang@outlook.com", 'utf-8')
-message['Subject'] = Header("标题", 'utf-8')
+# send message
+sender = jsonObj["sender"]
+receivers = jsonObj["receivers"]# bcc
+subject = jsonObj["subject"]
+content = jsonObj["content"]
+
+
+message = MIMEText(content, 'plain', 'utf-8')
+message['From'] = Header("邮件提醒", 'utf-8')
+message['Subject'] = Header(subject, 'utf-8')
+
 
 try:
-    smtpObj = smtplib.SMTP()
-    smtpObj.connect(email_server, email_server_port)
-    smtpObj.login(email_user, email_pass)
-    smtpObj.sendmail(sender, receivers, message.as_string())
+    for receiver in receivers:
+        print(receiver)
+        message['To'] = Header(receiver, 'utf-8')
+        smtpObj.sendmail(sender, receiver, message.as_string())
 except smtplib.SMTPException as e:
     print("Error:Send Email failed.%e"%e)
 
--- workflow.md
+++ workflow.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: Hector Jing
  * @Date: 2022-07-09 00:24:12
- * @LastEditTime: 2022-07-10 11:36:49
+ * @LastEditTime: 2022-07-10 23:23:37
  * @Description:
 -->
 my workflow:
@@ -12,3 +12,11 @@ help me tidy everything.
 数据管理：
 twitter,reddit->数据仓库，打标签；
 励志每一日一句
+配置文件加密
+如何管理自己的关键开发项目
+
+api集合整理
+电报推送以及发送消息给服务器；
+
+
+telegram,wechat<->json,oss,server
